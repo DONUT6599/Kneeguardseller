9360e3bb986f6b60268e11d72e492f10b83920c9
DONUT6599 <86889418+DONUT6599@users.noreply.github.com>
2025-12-21 21:51:10 +0700

    Create l.lua

---
+++ l.lua
@@ -0,0 +1,7 @@
+if game.GameId == 7488190691 then
+    if game.PlaceId == 133410800847665 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/DONUT6599/Kneeguardseller/refs/heads/main/Lob", true))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/DONUT6599/Kneeguardseller/refs/heads/main/ingame", true))()
+    end
+end
